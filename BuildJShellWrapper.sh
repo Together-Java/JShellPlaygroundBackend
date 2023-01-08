01a91141529abfa8e463791fb33fbdf70814e095
Taz03 <tanishazad03@gmail.com>
2023-01-08 15:36:23 +0530

    code fixes

--- BuildJShellWrapper.sh
+++ BuildJShellWrapper.sh
@@ -2,6 +2,7 @@
 cd JShellWrapper
 rm -rf out
 javac -d out/production/JShellWrapper src/main/java/*.java
-cp src/main/resources/META-INF/MANIFEST.MF out/production/JShellWrapper/META-INF/MANIFEST.MF
+mkdir out/production/JShellWrapper/META-INF
+cp src/main/resources/META-INF/MANIFEST.MF out/production/JShellWrapper/META-INF/
 cd out/production/JShellWrapper/
 jar -cfm ../../JShellWrapper.jar META-INF/MANIFEST.MF *.class
