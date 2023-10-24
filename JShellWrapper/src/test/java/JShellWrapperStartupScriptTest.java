b2328bc6eb439291227e8eb7110240d799d4b929
Alathreon <master64046@gmail.com>
2023-10-24 23:11:23 +0200

    Added tests for JShell Wrapper

---
+++ JShellWrapper/src/test/java/JShellWrapperStartupScriptTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
+
+class JShellWrapperStartupScriptTest {
+    @Test
+    void testDoubleSnippets() {
+        Config config = new Config(5);
+        StringInputStream inputStream = new StringInputStream("""
+                import java.util.*; void println(Object o) { System.out.println(o); }
+                eval
+                1
+                println(List.of("a", "b", "c"))
+                exit""");
+        UnboundStringOutputStream outputStream = new UnboundStringOutputStream();
+        JShellWrapper jshell = new JShellWrapper();
+        jshell.run(config, inputStream, new PrintStream(outputStream));
+        assertEquals("""
+                OK
+                1
+                VALID
+                ADDITION
+                3
+                println(List.of("a", "b", "c"))
+                
+                false
+                [a, b, c]\\n
+                OK
+                """, outputStream.readAll());
+    }
+}
---
+++ JShellWrapper/src/test/java/StringInputStream.java
@@ -0,0 +1,17 @@
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class StringInputStream extends InputStream {
+    private final byte[] content;
+    private int i;
+
+    public StringInputStream(String content) {
+        this.content = content.getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public int read() {
+        if(i == content.length) return -1;
+        return content[i++] & 0xFF;
+    }
+}
