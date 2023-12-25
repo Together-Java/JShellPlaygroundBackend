dc1a18105421b22a8ab0d76c966658ddabd62631
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2023-12-25 23:49:33 +0100

    Use docker java client

---
+++ JShellAPI/src/main/java/org/togetherjava/jshellapi/service/DockerService.java
@@ -0,0 +1,120 @@
+package org.togetherjava.jshellapi.service;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class DockerService implements AutoCloseable {
+
+    private final DockerClient client;
+
+    public DockerService() {
+        DefaultDockerClientConfig clientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        ApacheDockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(clientConfig.getDockerHost())
+                .sslConfig(clientConfig.getSSLConfig())
+                .responseTimeout(Duration.ofSeconds(60))
+                .connectionTimeout(Duration.ofSeconds(60))
+                .build();
+        this.client = DockerClientImpl.getInstance(clientConfig, httpClient);
+    }
+
+    public String spawnContainer(
+            long maxMemoryMegs, long cpus, String name, Duration evalTimeout, long sysoutLimit
+    ) throws InterruptedException {
+        String imageName = "togetherjava.org:5001/togetherjava/jshellwrapper";
+        boolean presentLocally = client.listImagesCmd()
+                .withImageNameFilter(imageName)
+                .exec()
+                .stream()
+                .anyMatch(it -> Arrays.asList(it.getRepoTags()).contains("master"));
+
+        if (!presentLocally) {
+            client.pullImageCmd(imageName)
+                    .withTag("master")
+                    .exec(new PullImageResultCallback())
+                    .awaitCompletion(5, TimeUnit.MINUTES);
+        }
+
+        return client.createContainerCmd(
+                        imageName + ":master"
+                )
+                .withHostConfig(
+                        HostConfig.newHostConfig()
+                                .withAutoRemove(true)
+                                .withInit(true)
+                                .withCapDrop(Capability.ALL)
+                                .withNetworkMode("none")
+                                .withPidsLimit(2000L)
+                                .withReadonlyRootfs(true)
+                                .withMemory(maxMemoryMegs * 1024 * 1024)
+                                .withCpuCount(cpus)
+                )
+                .withStdinOpen(true)
+                .withAttachStdin(true)
+                .withAttachStderr(true)
+                .withAttachStdout(true)
+                .withEnv("evalTimeoutSeconds=" + evalTimeout.toSeconds(), "sysOutCharLimit=" + sysoutLimit)
+                .withName(name)
+                .exec()
+                .getId();
+    }
+
+    public InputStream startAndAttachToContainer(String containerId, InputStream stdin) throws IOException {
+        PipedInputStream pipeIn = new PipedInputStream();
+        PipedOutputStream pipeOut = new PipedOutputStream(pipeIn);
+
+        client.attachContainerCmd(containerId)
+                .withLogs(true)
+                .withFollowStream(true)
+                .withStdOut(true)
+                .withStdErr(true)
+                .withStdIn(stdin)
+                .exec(new ResultCallback.Adapter<>() {
+                    @Override
+                    public void onNext(Frame object) {
+                        try {
+                            String payloadString = new String(object.getPayload(), StandardCharsets.UTF_8);
+                            if (object.getStreamType() == StreamType.STDOUT) {
+                                pipeOut.write(object.getPayload());
+                            } else {
+                                System.err.println(":( " + payloadString);
+                            }
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    }
+                });
+
+        client.startContainerCmd(containerId).exec();
+        return pipeIn;
+    }
+
+    public void killContainerByName(String name) {
+        for (Container container : client.listContainersCmd().withNameFilter(Set.of(name)).exec()) {
+            client.killContainerCmd(container.getId()).exec();
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        client.close();
+    }
+
+    public boolean isDead(String containerName) {
+        return client.listContainersCmd().withNameFilter(Set.of(containerName)).exec().isEmpty();
+    }
+}
